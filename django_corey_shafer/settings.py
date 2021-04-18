cf52439ef936b7ad09c410c8abd7e7969404a0bf
Mateusz Dziuba <mateusz.dziuba97@gmail.com>
2021-04-18 23:10:04 +0200

    Initial commit

--- django_corey_shafer/settings.py
+++ django_corey_shafer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://matdzapp.herokuapp.com']
+ALLOWED_HOSTS = ['matdzapp.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
